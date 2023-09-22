4ee87b80a00ee01aec8f246ad7118ed170850ad5
eros.montin <eros.montin@gmail.com>
2023-09-22 17:05:39 -0400

    v1

--- pyfe_eros_montin/utils.py
+++ pyfe_eros_montin/utils.py
@@ -28,6 +28,7 @@ class MakeJsonFe():
                 for pr,roi in enumerate(a):
                     for pv,value in enumerate(self.roisvalueslist):
                             line.append([roi,value,f'R:{pr:02d}_RV:{pv:02d}'])
+                            print(line[-1][-1])
                             
                 ROIS.append(line)
         #let's see how is the situation with the images and confs
@@ -35,10 +36,9 @@ class MakeJsonFe():
             IMAGES=[]
             for pa,a in enumerate(self.imageslist):
                 line=[]
-                for image in a:
-                    for conf in self.confslist:
-                            line.append([image,conf,f'I:{pr:02d}_C:{pv:02d}'])
-
+                for pi,image in enumerate(a):
+                    for pc,conf in enumerate(self.confslist):
+                            line.append([image,conf,f'I:{pi:02d}_C:{pc:02d}'])
                 IMAGES.append(line)
         OUT=[]
         if self.method['images_rois']=='cross':
@@ -75,29 +75,77 @@ if __name__=='__main__':
     # p=pf.exrtactMyFeaturesToPandas(o.getPosition(),2,3)
     # p.to_json('/g/extr.json')
 
+    # roilist=[]
+    # ids=[]
+    # imageslist=[]
+
+    # for tkr in ['TKR','NonTKR']:
+    #     S=pn.Pathable(f'/data/MYDATA/Eros_143TKR_143NonTKR/2_Label_Maps_Remapped/{tkr}/9003380.nii.gz')
+    #     for l in S.getFilesInPathByExtension()[0:2]:
+    #         L=pn.Pathable(l)
+    #         r=L.getPosition()
+    #         roilist.append([r])
+    #         L.renamePath('2_Label_Maps_Remapped','1_T2ValuesMaps')
+    #         if not L.exists():
+    #             #remove the roi because there's not the associated image
+    #             roilist.pop()
+    #             break
+    #         im=[]
+    #         im.append(L.getPosition())
+    #         ids.append(f'_{tkr}_{L.getFileName()}')
+    #         imageslist.append(im)
+
+
+
+
+    # DIMENSION=3
+    # method={'rois_roivalues':'cross','images_confs':'cross','images_rois':'cross'}
+    # A=MakeJsonFe(method=method)
+    # A.imageslist=imageslist
+    # A.roislist=roilist
+    # A.ids=ids
+    # omo={"min":0,"max":5000,"bin":32}
+    # omo2={"min":0,"max":5000,"bin":128}
+    # MO=[
+    # {"type":"FOS","options":omo,"name":"FOS32"},
+    #     # {"type":"FOS","options":omo2,"name":"FOS128"},
+    #     # {"type":"GLCM","options":omo,"name":"GLCM32"},
+    #     # {"type":"GLRLM","options":omo,"name":"GLRLM32"},
+    #     # {"type":"GLCM","options":omo2,"name":"GLCM32"},
+    #     # {"type":"GLRLM","options":omo2,"name":"GLRLM32"},
+    #     {"type":"SS","options":None,"name":"SS_1"}]
+    # A.confslist=[MO]
+    # # A.roisvalueslist=[10,20,30,40,50,60]
+    # A.roisvalueslist=[10,20]
+    # D=A.getDictionary()
+    # o=pn.Pathable('/g/feconf.json')
+    # o.ensureDirectoryExistence()
+    # o.writeJson({"dimension":DIMENSION,"dataset":D})
+    # p=exrtactMyFeaturesToPandas(o.getPosition(),DIMENSION,3)
+    # p.to_json('/g/extraction.json')
+
+    from glob import glob 
+    
+    import os
+    theimages=['fo.nii','IN.nii','OUT.nii','wo.nii']
+
+    P=['/data/PROJECTS/HIPSEGENTATION/RADIOMIC_HIP_2/FILES/C','/data/PROJECTS/HIPSEGENTATION/RADIOMIC_HIP_2/FILES/P']
+    PNAME=['subjects','patients']
     roilist=[]
     ids=[]
     imageslist=[]
 
-    for tkr in ['TKR','NonTKR']:
-        S=pn.Pathable(f'/data/MYDATA/Eros_143TKR_143NonTKR/2_Label_Maps_Remapped/{tkr}/9003380.nii.gz')
-        for l in S.getFilesInPathByExtension()[0:2]:
-            L=pn.Pathable(l)
-            r=L.getPosition()
+    for p,n in zip(P,PNAME):
+        for l in glob(p+"*", recursive = True)[0:2]:
+            r=os.path.join(l,'data/roi.nii.gz')
             roilist.append([r])
-            L.renamePath('2_Label_Maps_Remapped','1_T2ValuesMaps')
-            if not L.exists():
-                #remove the roi because there's not the associated image
-                roilist.pop()
-                break
-            im=[]
-            im.append(L.getPosition())
-            ids.append(f'_{tkr}_{L.getFileName()}')
-            imageslist.append(im)
+            imageslist.append([os.path.join(l,'data',im) for im in theimages])
+            ids.append(f'_{n}_{l.replace(p[:-2],"")}')
 
 
-
-
+    print('h')
+    EXTRACTION='CONF/extraction.json'
+    P= pn.Pathable('CONF/001/results.csv')
     DIMENSION=3
     method={'rois_roivalues':'cross','images_confs':'cross','images_rois':'cross'}
     A=MakeJsonFe(method=method)
@@ -108,21 +156,15 @@ if __name__=='__main__':
     omo2={"min":0,"max":5000,"bin":128}
     MO=[
     {"type":"FOS","options":omo,"name":"FOS32"},
-        # {"type":"FOS","options":omo2,"name":"FOS128"},
-        # {"type":"GLCM","options":omo,"name":"GLCM32"},
-        # {"type":"GLRLM","options":omo,"name":"GLRLM32"},
-        # {"type":"GLCM","options":omo2,"name":"GLCM32"},
-        # {"type":"GLRLM","options":omo2,"name":"GLRLM32"},
+        {"type":"FOS","options":omo2,"name":"FOS128"},
+        {"type":"GLCM","options":omo,"name":"GLCM32"},
+        {"type":"GLRLM","options":omo,"name":"GLRLM32"},
+        {"type":"GLCM","options":omo2,"name":"GLCM128"},
+        {"type":"GLRLM","options":omo2,"name":"GLRLM128"},
         {"type":"SS","options":None,"name":"SS_1"}]
     A.confslist=[MO]
-    # A.roisvalueslist=[10,20,30,40,50,60]
-    A.roisvalueslist=[10,20]
+    A.roisvalueslist=[1,2]
     D=A.getDictionary()
-    o=pn.Pathable('/g/feconf.json')
+    o=pn.Pathable('CONF/feconf.json')
     o.ensureDirectoryExistence()
     o.writeJson({"dimension":DIMENSION,"dataset":D})
-    p=exrtactMyFeaturesToPandas(o.getPosition(),DIMENSION,3)
-    p.to_json('/g/extraction.json')
-
-
-
