45a0d8cfcbfafe2fdd745d659ea8d6b9b837b747
eros.montin <eros.montin@gmail.com>
2023-10-10 18:14:28 -0400

    toml_

--- pyfe_eros_montin/utils.py
+++ pyfe_eros_montin/utils.py
@@ -1,10 +1,10 @@
 import toml
 
-with open('pyproject.toml', 'r') as f:
-    config = toml.load(f)
+# with open('pyproject.toml', 'r') as f:
+#     config = toml.load(f)
  
-# Access values from the config
-VERSION=config['project']['version']
+# # Access values from the config
+# VERSION=config['project']['version']
 
 from itertools import permutations
 omo={"min":0,"max":32000,"bin":32}
